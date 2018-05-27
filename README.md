c5dffbb6160b1992613c80dfc1fba02cecf6c24d
John Snow <thelunaticscripter@outlook.com>
2018-05-27 16:14:40 -0500

    Update LGPO to remove the binaries
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# LGPO Habitat Plan
+
+This is an example habitat plan that uses the LGPO binaries from Microsoft to restore a GPO backup to a computer.
--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -3,11 +3,9 @@ $pkg_origin="thelunaticscripter"
 $pkg_version="0.1.0"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@("Apache-2.0")
-# $pkg_filename="$pkg_name-$pkg_version.zip"
-# $pkg_shasum="TODO"
+$pkg_deps=@("thelunaticscripter/LGPO")
+$pkg_bin_dirs=@("bin")
 
 function Invoke-Install {
-  mkdir "$pkg_prefix/bin"
-  Copy-Item -Path "$PLAN_CONTEXT/../LGPO.exe" -Destination "$pkg_prefix/bin"
   Copy-Item -Path "$PLAN_CONTEXT/../{57E527AA-78D4-4CEF-8070-317768CB2813}" -Destination "$pkg_prefix/bin" -Recurse -Force
 }
