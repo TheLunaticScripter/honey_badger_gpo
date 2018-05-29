d28ee2bc316af4ec79c471f32959dce30db2fd79
John Snow <thelunaticscripter@outlook.com>
2018-05-29 09:13:31 -0400

    Change plan pointing to
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -3,7 +3,7 @@ $pkg_origin="thelunaticscripter"
 $pkg_version="0.1.0"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@("Apache-2.0")
-$pkg_deps=@("thelunaticscripter/LGPO")
+$pkg_deps=@("thelunaticscripter/lgpo")
 $pkg_bin_dirs=@("bin")
 
 function Invoke-Install {
